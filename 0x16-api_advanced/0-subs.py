5b46e97fa43e79ca6abab2b8dde89dba1fd8526e
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2024-08-13 22:18:41 +0200

    working on my mysubs fixes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """working to Return the total num of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'my-reddit-subscriber-script/0.1'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
