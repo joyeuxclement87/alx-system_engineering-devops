2153824424cf18946ab916d3f650de859f3ce539
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2024-08-13 22:27:34 +0200

    working on my mysubs fixes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 """
-working on the Script that queries subscribers on a given Reddit subreddit.
+This module provides a function to query the Reddit API and retrieve the number of subscribers for a given subreddit.
+
+Functions:
+    number_of_subscribers(subreddit): Returns the number of subscribers for the specified subreddit.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """working to Return the total num of subscribers on a given subreddit."""
+    """
+    Queries the Reddit API to return the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers for the subreddit. Returns 0 if the subreddit is invalid.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'my-reddit-subscriber-script/0.1'}
     response = requests.get(url, headers=headers, allow_redirects=False)
