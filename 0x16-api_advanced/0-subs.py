4a8baf895fc925bb9c01149b2fff4acce7208594
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2024-08-13 22:05:25 +0200

    working on my mysubs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
 """
-workin on the Module to the number of subscribers for a given Reddit subreddit.
+working on the Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the total number of subscribers on a given subreddit.
-
-    Args:
-        subreddit (str): The subreddit name.
-
-    Returns:
-        int: The number of subscribers if the subreddit exists, 0 otherwise.
-    """
+    """working to Return the total num of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data.get('data', {}).get('subscribers', 0)
-    except requests.RequestException:
-        pass
-    return 0
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
